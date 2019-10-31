d5198856618324b6c8652121bf53096e6bd11caa
Jeon Guk Bae <rnrqo9446@gmail.com>
2019-10-31 20:32:21 +0900

    subject

--- prj/settings.py
+++ prj/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+'PAGE_SIZE':1
+
+}
